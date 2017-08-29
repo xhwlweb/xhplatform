203b7727b15af593141110da237376bfa04bff4c
Xuancode <xuanplang@163.com>
2017-08-30 01:34:09 +0800

    基本构建

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+const Hello = r => require.ensure([], () => r(require('@/pages/Hello')), 'Hello')	//统一用懒加载方式
 
 Vue.use(Router)
 
